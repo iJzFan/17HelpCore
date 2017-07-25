a3391ecc656b474efe2f7c1e7507c623ea46c9f1
建中 范 <jz.fan@qq.com>
2017-07-25 22:05:47 +0800

    添加项目文件。

---
+++ Responsible/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Responsible.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
