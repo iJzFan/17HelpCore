b72307173263ea2246cfcb963a3626d77d89c480
建中 范 <jz.fan@qq.com>
2017-08-30 21:54:14 +0800

    Commit Areas

---
+++ Responsible/Areas/API/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HELP.Service.ViewModel.Log;
+using Users=HELP.GlobalFile.Global.Story.User;
+
+namespace Responsible.Areas.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
+    {
+        public OnModel GET()
+        {
+            return new OnModel {UserName= Users.yezi_UserName,Password=Users.yezi_PassWord,ImageCode=null };
+        }
+    }
+}
