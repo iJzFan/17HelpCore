cb484d0cfa784165f293a3e5c079d434f78af9a9
建中 范 <jz.fan@qq.com>
2017-08-30 21:52:54 +0800

    Commit WeHhelp

---
+++ Responsible/WebHelp/ReturnUrlHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HELP.UI.Responsible.WebHelp
+{
+    public class ReturnUrlHelper
+    {
+        public static IActionResult ReturnUrl(string returnUrl)
+        {
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                return new RedirectResult(returnUrl);
+            }
+
+            return new RedirectResult("/Problem/Index");
+        }
+    }
+}
