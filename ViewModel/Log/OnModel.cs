2e9c1f9d5f026f277f03fff4989aefe0aec6d15c
建中 范 <jz.fan@qq.com>
2017-09-03 14:37:28 +0800

    Commit Log

---
+++ ViewModel/Log/OnModel.cs
@@ -0,0 +1,21 @@
+﻿using HELP.Service.ViewModel.Shared;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HELP.Service.ViewModel.Log
+{
+    public class OnModel
+    {
+        [Required]
+        [DisplayName("用户名")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "*用户名长度只能是4~20个字符")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "*密码长度只能是4~20个字符")]
+        [DisplayName("密码")]
+        public string Password { get; set; }
+
+        public ImageCodeModel ImageCode { get; set; }
+    }
+}
