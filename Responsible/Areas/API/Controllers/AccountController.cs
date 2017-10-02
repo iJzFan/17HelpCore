481d714d658feab010a037e3a19ccf65f9539d10
建中 范 <jz.fan@qq.com>
2017-10-02 09:25:27 +0800

    细节修改

--- Responsible/Areas/API/Controllers/AccountController.cs
+++ Responsible/Areas/API/Controllers/AccountController.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using HELP.Service.ViewModel.Log;
 using Microsoft.AspNetCore.Mvc;
-using HELP.Service.ViewModel.Log;
 using Users=HELP.GlobalFile.Global.Story.User;
 
-namespace Responsible.Areas.API.Controllers
+namespace HELP.UI.Responsible.Areas.API.Controllers
 {
     [Produces("application/json")]
     [Route("api/Account")]
     public class AccountController : Controller
     {
-        public OnModel GET()
+        public OnModel Get()
         {
-            return new OnModel {UserName= Users.yezi_UserName,Password=Users.yezi_PassWord,ImageCode=null };
+            return new OnModel {UserName = Users.yezi_UserName, Password = Users.yezi_PassWord, ImageCode = null};
         }
     }
 }
